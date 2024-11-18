ddaffec7b2dce4835f1a498d46bd3fb8824cf4fb
Lucy <lucy@nix2twink.gay>
2024-11-18 22:41:13 +0100

    added Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,12 +7,24 @@ const Skills: React.FC = () => {
         <section className="tech-skills">
             <h2>Tech Skills</h2>
             <div className="skills-grid">
-                {skills.map((skill, index) => (
-                    <div className="skill-card" key={index}>
-                        <h3>{skill}</h3>
-                        <p>Advanced configuration and system management</p>
+                    <div className="skill-card">
+                        <h3>Linux</h3>
+                        <p>I already used most of the common linux distributions in the desktop and also in the server
+                            section</p>
                     </div>
-                ))}
+                <div className="skill-card">
+                    <h3>Network & WIFI-Mesh</h3>
+                    <p>I already played around with OpenWRT and pfSense, also i installed networks for Freifunk Leipzig and administrating them.</p>
+                </div>
+                <div className="skill-card">
+                    <h3>Kotlin/Java</h3>
+                </div>
+                <div className="skill-card">
+                    <h3>C/C++</h3>
+                </div>
+                <div className="skill-card">
+                    <h3>Nix</h3>
+                </div>
             </div>
         </section>
     );
